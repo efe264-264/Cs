95ab7a746588f004faa117383409ade7ae31bd80
efe264-264 <154609801+efe264-264@users.noreply.github.com>
2025-02-20 21:55:21 +0800

    Add files via upload

---
+++ assignment1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace RG1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 0;
+            Console.WriteLine("请输入第一个数字");
+            double Op1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("请输入第二个数字");
+            double Op2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("请输入运算符");
+            char Operator = char.Parse(Console.ReadLine());
+            switch (Operator)
+            {
+                case '+':
+                    result = Op1 + Op2;
+                    break;
+                case '-':
+                    result = Op1 - Op2;
+                    break;
+                case '*':
+                    result = Op1 * Op2;
+                    break;
+                case '/':
+                    result = Op1 / Op2;
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    return;
+            }
+
+            Console.WriteLine("\n结果是：");
+            Console.WriteLine(result);
+        }
+    }
+}
