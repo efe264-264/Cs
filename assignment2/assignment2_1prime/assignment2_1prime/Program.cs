9d8f8702b788ed8f2d8e323374c0432b82baddc0
efe264-264 <154609801+efe264-264@users.noreply.github.com>
2025-02-27 12:07:51 +0800

    Add files via upload

---
+++ assignment2/assignment2_1prime/assignment2_1prime/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace assignment2_1prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = GetInput();
+           List<int> myPrime= Prime(number);
+            
+            Console.WriteLine($"整数 {number} 的素数因子为：{string.Join(" ", myPrime)}");
+        }
+        // 获取用户输入的合法整数
+        static int GetInput()
+        {
+            int number;
+            while (true)
+            {
+                Console.Write("请输入你的数字：");
+                string input = Console.ReadLine();
+
+                // 验证输入是否为有效整数且大于1
+                if (int.TryParse(input, out number)/*转换类型成功*/ )
+                {
+                    if(number > 1)
+                    return number;
+                    else
+                    {
+                        Console.WriteLine("输入不大于1，非质数！");
+                    }
+                }
+                Console.WriteLine("输入无效");
+            }
+        }
+
+      
+        static List<int> Prime(int n)
+        {
+           //用集合存储所有的质数因子
+            HashSet<int> myPrime = new HashSet<int>();
+            //计算部分
+            while (n % 2 == 0)//首先查找2的
+            {
+                myPrime.Add(2);
+                n /= 2;
+            }
+
+            
+            for (int i = 3; i * i <= n; i += 2)//然后再找所有奇数
+            {
+                while (n % i == 0)
+                {
+                    myPrime.Add(i);
+                    n /= i;
+                }
+            }
+
+            
+            if (n > 2)//最后剩下的也是
+            {
+                myPrime.Add(n);
+            }
+
+            return myPrime.ToList();
+        }
+    }
+}
---
+++ assignment2/assignment2_2Arr/assignment2_2Arr/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace assignment2_2Arr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myArr = GetInput();
+            DoOutput(myArr);
+        }
+
+        static List<int>  GetInput()
+        {
+            List<int> myArr = new List<int>();
+
+            int number;
+            
+                Console.Write("请输入你的数组，用空格分隔数字：");
+                string input = Console.ReadLine();
+                string[] inputString = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach(string numberString in inputString )
+                {
+                    if (int.TryParse(numberString, out number)) 
+                     myArr.Add(number); 
+                    else 
+                     Console.Write("输入错误");
+                }
+            
+            return myArr;
+        }
+        static void DoOutput(List<int> myArr)
+        {
+            Console.Write("最大数="); Console.Write(GetArrMax(myArr)); Console.Write('\n');
+            Console.Write("最小数="); Console.Write(GetArrMin(myArr)); Console.Write('\n');
+            Console.Write("总和="); Console.Write(GetArrSum(myArr)); Console.Write('\n');
+            Console.Write("平均数="); Console.Write(GetArrAve(myArr)); Console.Write('\n');
+        }
+
+        static int GetArrMax(List<int> myArr)
+        {
+            int MaxNumber=myArr.Max();
+            return MaxNumber;
+        }
+        static int GetArrMin(List<int> myArr)
+        {
+            int MinNumber = myArr.Min();
+            return MinNumber;
+        }
+        static double GetArrAve(List<int> myArr)
+        {
+            double average = myArr.Average();
+            return average;
+        }
+        static int GetArrSum(List<int> myArr)
+        {
+            int SumNumber = myArr.Sum();
+           
+            return SumNumber;
+        }
+
+
+    }
+}
