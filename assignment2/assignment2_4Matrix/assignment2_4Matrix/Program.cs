fdc0c345599090c7fc474da93128bdfe3e68b852
efe264-264 <154609801+efe264-264@users.noreply.github.com>
2025-02-27 19:55:18 +0800

    Add files via upload

---
+++ assignment2/assignment2_4Matrix/assignment2_4Matrix/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace assignment2_4Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] matrix1 = {
+            new int[] {1, 2, 3, 4},
+            new int[] {5, 1, 2, 3},
+            new int[] {9, 5, 1, 2}
+            };
+            int[][] matrix2 = {
+            new int[] {1, 5, 3, 4},
+            new int[] {5, 1, 2, 3},
+            new int[] {9, 5, 1, 2}
+            };
+
+            Console.Write(IsTopliMatrix(matrix1));
+
+            Console.Write('\n');
+            Console.Write(IsTopliMatrix(matrix2));
+        }
+
+
+
+        static bool IsTopliMatrix(int[][] matrix)
+            {
+                int row = matrix.Length;
+                int col = matrix[0].Length;
+
+                for (int i = 0; i < row-1; i++)
+                {
+                    
+                    for (int j = 0; j < row-1; j++)
+                    {
+                        if (matrix[i][j] != matrix[i + 1][j + 1])
+                            return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+
